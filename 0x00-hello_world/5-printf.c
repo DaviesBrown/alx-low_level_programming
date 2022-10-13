40419912e0dfdb0f94fa7114da4d2ef43a77fb4f
DavidBrown <nwosudavid77@gmail.com>
2022-10-13 08:28:04 -0700

    answer

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	fprintf(stderr, "[Anything]");
-return (0);
+	return (0);
 }

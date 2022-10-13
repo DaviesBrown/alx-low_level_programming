71967197ed9947754efda38a084e018dc587db77
DavidBrown <nwosudavid77@gmail.com>
2022-10-13 03:27:53 -0700

    task 5 on the line

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+/* Print with printf */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	fprintf(stderr,"[Anything]");
+	fprintf(stderr, "[Anything]");
 return (0);
 }

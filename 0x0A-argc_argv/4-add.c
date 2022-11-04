9339675075ba9ca430cc225d69d7223dd05486af
DavidBrown <nwosudavid77@gmail.com>
2022-11-04 14:43:37 -0700

    argc vs argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument value
+ */
+
+int main(int argc, char **argv)
+{
+	int i, add;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] >= '0' && *argv[i] <= '9')
+		{
+			add += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	if (add > 0)
+		printf("%d\n", add);
+	return (0);
+}

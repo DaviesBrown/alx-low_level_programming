d4df1fe5842e7efca0e37533dc972508fe86b852
DavidBrown <nwosudavid77@gmail.com>
2022-12-05 23:51:09 -0800

    hot fix

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,7 +3,7 @@
 
 /*
  * File: holberton.h
- * Auth: Godswill Kalu
+ * Auth: david brown
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */

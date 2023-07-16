3eee63d6c534568513eb16401c78f16c21b171f7
Davies Brown <nwosudavid77.com>
2023-07-16 21:42:03 +0100

    search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,32 +1,48 @@
-#include <math.h>
 #include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in an array of integers
+ * @array: array of integers
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: the index of @value or -1 if not found or if @array is NULL
+ */
 
 int jump_search(int *array, size_t size, int value)
 {
-    int i = 0;
-    int j = sqrt(size);
+	size_t i, step, shift, l;
 
-    printf("Value checked array[%d] = [%d]\n", i, array[i]);
-    while (array[j - 1] < value && j < (int)size)
-    {
-        i = j;
-        j += sqrt(size);
-    printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        if (i >= (int)size) {
-            return -1;
-        }
-    }
-    printf("Value found between indexes [%d] and [%d]\n", i, j);
-    while (array[i] < value) {
-        printf("Value checked array[%d] = [%d]\n", i, array[i]);
-        i = i + 1;
-        if (i == j) {
-            return -1;
-        }
-    }
-    if (array[i] == value) {
-        return i;
-    } else {
-        return -1;
-    }
+	step = sqrt(size);
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	i = shift = 0;
+
+	while (shift < size && array[shift] < value)
+	{
+		printf("Value checked array[%ld] = [%d]\n", shift, array[shift]);
+		i = shift;
+		shift += step;
+	}
+	printf("Value found between indexes [%ld] and [%ld]\n", i, shift);
+
+	if (shift >= size)
+		shift = size - 1;
+
+	for (; i < shift && array[i] < value; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	}
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+
+	for (l = 0; l < size; l++)
+	{
+		if (array[l] == value)
+			return (l);
+	}
+
+	return (-1);
 }
--- 0x1E-search_algorithms/README.md
+++ 0x1E-search_algorithms/README.md
@@ -1,1 +1,11 @@
-search_algorithms
+# Search Algorithms
+
+## Search Methods
+<ol>
+<li>Linear Search</li>
+<li>Binary Search</li>
+<li>Jump Search</li>
+<li>Interpolation Search</li>
+<li>Exponential Search</li>
+<li>Advanced Binary Search</li>
+</ol>
